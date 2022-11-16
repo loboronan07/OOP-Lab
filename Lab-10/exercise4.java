814459c98acc693c5ecf047771000d230b9cef94
Ronan Lobo <113038897+loboronan07@users.noreply.github.com>
2022-11-16 22:57:17 +0530

    Indentation Error Corrected

--- Lab-10/exercise4.java
+++ Lab-10/exercise4.java
@@ -19,27 +19,27 @@ class exercise4 {
         int n;
 
         System.out.print("Enter the number of elements in the integer array: ");
-		n = sc.nextInt();
+	n = sc.nextInt();
 
-		Integer[] intArr = new Integer[n];
+	Integer[] intArr = new Integer[n];
 
-		System.out.print("Enter the elements of the array: ");
-		for(int i=0; i<n; i++)
-			intArr[i] = sc.nextInt();
+	System.out.print("Enter the elements of the array: ");
+	for(int i=0; i<n; i++)
+		intArr[i] = sc.nextInt();
 
-		GenArr<Integer> GenInt = new GenArr<Integer>(intArr);
+	GenArr<Integer> GenInt = new GenArr<Integer>(intArr);
 
-		System.out.print("Enter the number of elements in the String array: ");
-		n = sc.nextInt();
+	System.out.print("Enter the number of elements in the String array: ");
+	n = sc.nextInt();
 
-		String[] strArr = new String[n];
+	String[] strArr = new String[n];
 
-		System.out.println("Enter the elements of the array: ");
-		sc.nextLine();
-		for(int i=0; i<n; i++)
-			strArr[i] = sc.nextLine();
+	System.out.println("Enter the elements of the array: ");
+	sc.nextLine();
+	for(int i=0; i<n; i++)
+		strArr[i] = sc.nextLine();
 
-		GenArr<String> GenStr = new GenArr<String>(strArr);
+	GenArr<String> GenStr = new GenArr<String>(strArr);
 
         System.out.println("\n\nValues in Integer Array are: ");
         display(GenInt);
